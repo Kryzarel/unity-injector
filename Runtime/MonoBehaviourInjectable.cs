8c427ab330d3cfa609355e20ad2255d1d56b3ac2
Kryzarel <8752418+Kryzarel@users.noreply.github.com>
2024-08-18 00:42:22 +0100

    Actual first commit. Added package manifest, MonoBehaviourInjectable class and some other stuff

---
+++ Runtime/MonoBehaviourInjectable.cs
@@ -0,0 +1,205 @@
+using Kryz.DI;
+using UnityEngine;
+
+namespace Kryz.MonoDI
+{
+	public abstract class MonoBehaviourInjectable : MonoBehaviour
+	{
+		public virtual void Awake()
+		{
+			DependencyInjector.RootContainer.Inject(this);
+		}
+	}
+
+	public abstract class MonoBehaviour<T1> : MonoBehaviourInjectable
+	{
+		[Inject]
+		protected abstract void Init(T1 arg1);
+	}
+
+	public abstract class MonoBehaviour<T1, T2> : MonoBehaviourInjectable
+	{
+		[Inject]
+		protected abstract void Init(T1 arg1, T2 arg2);
+	}
+
+	public abstract class MonoBehaviour<T1, T2, T3> : MonoBehaviourInjectable
+	{
+		[Inject]
+		protected abstract void Init(T1 arg1, T2 arg2, T3 arg3);
+	}
+
+	public abstract class MonoBehaviour<T1, T2, T3, T4> : MonoBehaviourInjectable
+	{
+		[Inject]
+		protected abstract void Init(T1 arg1, T2 arg2, T3 arg3, T4 arg4);
+	}
+
+	public abstract class MonoBehaviour<T1, T2, T3, T4, T5> : MonoBehaviourInjectable
+	{
+		[Inject]
+		protected abstract void Init(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5);
+	}
+
+	public abstract class MonoBehaviour<T1, T2, T3, T4, T5, T6> : MonoBehaviourInjectable
+	{
+		[Inject]
+		protected abstract void Init(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6);
+	}
+
+	public abstract class MonoBehaviour<T1, T2, T3, T4, T5, T6, T7> : MonoBehaviourInjectable
+	{
+		[Inject]
+		protected abstract void Init(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7);
+	}
+
+	public abstract class MonoBehaviour<T1, T2, T3, T4, T5, T6, T7, T8> : MonoBehaviourInjectable
+	{
+		[Inject]
+		protected abstract void Init(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8);
+	}
+
+	public abstract class MonoBehaviour<T1, T2, T3, T4, T5, T6, T7, T8, T9> : MonoBehaviourInjectable
+	{
+		[Inject]
+		protected abstract void Init(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9);
+	}
+
+	public abstract class MonoBehaviour<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> : MonoBehaviourInjectable
+	{
+		[Inject]
+		protected abstract void Init(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10);
+	}
+
+	public abstract class MonoBehaviour<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11> : MonoBehaviourInjectable
+	{
+		[Inject]
+		protected abstract void Init(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11);
+	}
+
+	public abstract class MonoBehaviour<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12> : MonoBehaviourInjectable
+	{
+		[Inject]
+		protected abstract void Init(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12);
+	}
+
+	public abstract class MonoBehaviour<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13> : MonoBehaviourInjectable
+	{
+		[Inject]
+		protected abstract void Init(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12, T13 arg13);
+	}
+
+	public abstract class MonoBehaviour<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14> : MonoBehaviourInjectable
+	{
+		[Inject]
+		protected abstract void Init(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12, T13 arg13, T14 arg14);
+	}
+
+	public abstract class MonoBehaviour<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15> : MonoBehaviourInjectable
+	{
+		[Inject]
+		protected abstract void Init(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12, T13 arg13, T14 arg14, T15 arg15);
+	}
+
+	public abstract class MonoBehaviour<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16> : MonoBehaviourInjectable
+	{
+		[Inject]
+		protected abstract void Init(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12, T13 arg13, T14 arg14, T15 arg15, T16 arg16);
+	}
+
+	public abstract class MonoBehaviour<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17> : MonoBehaviourInjectable
+	{
+		[Inject]
+		protected abstract void Init(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12, T13 arg13, T14 arg14, T15 arg15, T16 arg16, T17 arg17);
+	}
+
+	public abstract class MonoBehaviour<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18> : MonoBehaviourInjectable
+	{
+		[Inject]
+		protected abstract void Init(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12, T13 arg13, T14 arg14, T15 arg15, T16 arg16, T17 arg17, T18 arg18);
+	}
+
+	public abstract class MonoBehaviour<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19> : MonoBehaviourInjectable
+	{
+		[Inject]
+		protected abstract void Init(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12, T13 arg13, T14 arg14, T15 arg15, T16 arg16, T17 arg17, T18 arg18, T19 arg19);
+	}
+
+	public abstract class MonoBehaviour<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20> : MonoBehaviourInjectable
+	{
+		[Inject]
+		protected abstract void Init(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12, T13 arg13, T14 arg14, T15 arg15, T16 arg16, T17 arg17, T18 arg18, T19 arg19, T20 arg20);
+	}
+
+	public abstract class MonoBehaviour<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21> : MonoBehaviourInjectable
+	{
+		[Inject]
+		protected abstract void Init(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12, T13 arg13, T14 arg14, T15 arg15, T16 arg16, T17 arg17, T18 arg18, T19 arg19, T20 arg20, T21 arg21);
+	}
+
+	public abstract class MonoBehaviour<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22> : MonoBehaviourInjectable
+	{
+		[Inject]
+		protected abstract void Init(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12, T13 arg13, T14 arg14, T15 arg15, T16 arg16, T17 arg17, T18 arg18, T19 arg19, T20 arg20, T21 arg21, T22 arg22);
+	}
+
+	public abstract class MonoBehaviour<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22, T23> : MonoBehaviourInjectable
+	{
+		[Inject]
+		protected abstract void Init(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12, T13 arg13, T14 arg14, T15 arg15, T16 arg16, T17 arg17, T18 arg18, T19 arg19, T20 arg20, T21 arg21, T22 arg22, T23 arg23);
+	}
+
+	public abstract class MonoBehaviour<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24> : MonoBehaviourInjectable
+	{
+		[Inject]
+		protected abstract void Init(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12, T13 arg13, T14 arg14, T15 arg15, T16 arg16, T17 arg17, T18 arg18, T19 arg19, T20 arg20, T21 arg21, T22 arg22, T23 arg23, T24 arg24);
+	}
+
+	public abstract class MonoBehaviour<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25> : MonoBehaviourInjectable
+	{
+		[Inject]
+		protected abstract void Init(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12, T13 arg13, T14 arg14, T15 arg15, T16 arg16, T17 arg17, T18 arg18, T19 arg19, T20 arg20, T21 arg21, T22 arg22, T23 arg23, T24 arg24, T25 arg25);
+	}
+
+	public abstract class MonoBehaviour<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25, T26> : MonoBehaviourInjectable
+	{
+		[Inject]
+		protected abstract void Init(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12, T13 arg13, T14 arg14, T15 arg15, T16 arg16, T17 arg17, T18 arg18, T19 arg19, T20 arg20, T21 arg21, T22 arg22, T23 arg23, T24 arg24, T25 arg25, T26 arg26);
+	}
+
+	public abstract class MonoBehaviour<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25, T26, T27> : MonoBehaviourInjectable
+	{
+		[Inject]
+		protected abstract void Init(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12, T13 arg13, T14 arg14, T15 arg15, T16 arg16, T17 arg17, T18 arg18, T19 arg19, T20 arg20, T21 arg21, T22 arg22, T23 arg23, T24 arg24, T25 arg25, T26 arg26, T27 arg27);
+	}
+
+	public abstract class MonoBehaviour<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25, T26, T27, T28> : MonoBehaviourInjectable
+	{
+		[Inject]
+		protected abstract void Init(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12, T13 arg13, T14 arg14, T15 arg15, T16 arg16, T17 arg17, T18 arg18, T19 arg19, T20 arg20, T21 arg21, T22 arg22, T23 arg23, T24 arg24, T25 arg25, T26 arg26, T27 arg27, T28 arg28);
+	}
+
+	public abstract class MonoBehaviour<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25, T26, T27, T28, T29> : MonoBehaviourInjectable
+	{
+		[Inject]
+		protected abstract void Init(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12, T13 arg13, T14 arg14, T15 arg15, T16 arg16, T17 arg17, T18 arg18, T19 arg19, T20 arg20, T21 arg21, T22 arg22, T23 arg23, T24 arg24, T25 arg25, T26 arg26, T27 arg27, T28 arg28, T29 arg29);
+	}
+
+	public abstract class MonoBehaviour<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25, T26, T27, T28, T29, T30> : MonoBehaviourInjectable
+	{
+		[Inject]
+		protected abstract void Init(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12, T13 arg13, T14 arg14, T15 arg15, T16 arg16, T17 arg17, T18 arg18, T19 arg19, T20 arg20, T21 arg21, T22 arg22, T23 arg23, T24 arg24, T25 arg25, T26 arg26, T27 arg27, T28 arg28, T29 arg29, T30 arg30);
+	}
+
+	public abstract class MonoBehaviour<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25, T26, T27, T28, T29, T30, T31> : MonoBehaviourInjectable
+	{
+		[Inject]
+		protected abstract void Init(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12, T13 arg13, T14 arg14, T15 arg15, T16 arg16, T17 arg17, T18 arg18, T19 arg19, T20 arg20, T21 arg21, T22 arg22, T23 arg23, T24 arg24, T25 arg25, T26 arg26, T27 arg27, T28 arg28, T29 arg29, T30 arg30, T31 arg31);
+	}
+
+	public abstract class MonoBehaviour<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25, T26, T27, T28, T29, T30, T31, T32> : MonoBehaviourInjectable
+	{
+		[Inject]
+		protected abstract void Init(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12, T13 arg13, T14 arg14, T15 arg15, T16 arg16, T17 arg17, T18 arg18, T19 arg19, T20 arg20, T21 arg21, T22 arg22, T23 arg23, T24 arg24, T25 arg25, T26 arg26, T27 arg27, T28 arg28, T29 arg29, T30 arg30, T31 arg31, T32 arg32);
+	}
+}
---
+++ Tests/MonoBehaviourInjectableTests.cs
@@ -0,0 +1,8 @@
+using NUnit.Framework;
+
+namespace Kryz.MonoDI.Tests
+{
+	public class MonoBehaviourInjectableTests
+	{
+	}
+}
